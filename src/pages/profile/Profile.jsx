e2db607450f9350e69d2afc4ecb77668688f8867
Tha-real-misfit <chappy4986@gmail.com>
2023-01-12 10:34:06 +1030

    upto creating profile page

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,7 +1,24 @@
 import "./profile.scss";
+import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import PlaceIcon from "@mui/icons-material/Place";
+import LanguageIcon from "@mui/icons-material/Language";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Posts from "../../components/posts/Posts";
 
 const Profile = () => {
-  return <div className="profile">Profile</div>;
+  return (
+    <div className="profile">
+      <div className="images">
+        <img src="" alt="" className="cover" />
+        <img src="" alt="" className="profilePic" />
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
