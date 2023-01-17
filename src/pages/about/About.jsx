9ea72f2ddc937c1846146000b6d2244fefe61321
Tha-real-misfit <chappy4986@gmail.com>
2023-01-17 15:43:04 +1030

    styling about us

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -27,41 +27,51 @@ const About = () => {
       </div>
       <div className="aboutContainer">
         <div className="mainInfo">
-          <div className="left">
-            <a href="http://facebook.com">
-              <FacebookTwoToneIcon fontSize="small" />
-            </a>
-            <a href="http://instagram.com">
-              <InstagramIcon fontSize="small" />
-            </a>
-            <a href="http://twitter.com">
-              <TwitterIcon fontSize="small" />
-            </a>
-            <a href="http://linkedIn.com">
-              <LinkedInIcon fontSize="small" />
-            </a>
-            <a href="http://pinterest.com">
-              <PinterestIcon fontSize="small" />
-            </a>
-          </div>
-          <div className="center">
-            <span>About Us</span>
-            <div className="info">
-              <div className="item">
-                <a href="https://goo.gl/maps/gtGA4WM3fhAXthjV6">
-                  <PlaceIcon />
-                </a>
-                <span>Adelaide, Australia</span>
+          {/* <span>About Us</span> */}
+          <div className="mainLeft">
+            <div className="left">
+              <a href="http://facebook.com">
+                <FacebookTwoToneIcon fontSize="small" />
+              </a>
+              <a href="http://instagram.com">
+                <InstagramIcon fontSize="small" />
+              </a>
+              <a href="http://twitter.com">
+                <TwitterIcon fontSize="small" />
+              </a>
+              <a href="http://linkedIn.com">
+                <LinkedInIcon fontSize="small" />
+              </a>
+              <a href="http://pinterest.com">
+                <PinterestIcon fontSize="small" />
+              </a>
+            </div>
+            <div className="center">
+              <div className="info">
+                <div className="item">
+                  <a href="https://goo.gl/maps/gtGA4WM3fhAXthjV6">
+                    <PlaceIcon />
+                  </a>
+                  <span>Adelaide, Australia</span>
+                </div>
               </div>
             </div>
+            <div className="right">
+              <EmailOutlinedIcon />
+              <MoreVertIcon />
+            </div>
           </div>
-          <div className="right">
-            <EmailOutlinedIcon />
-            <MoreVertIcon />
+          <div className="mainRight">
+            <span className="rightInfo">
+              We are a small family operated vet. We started out in Adelaide,
+              Austrlia in 1986 and have been here ever since. We welcome all
+              types of pets but specialise in dogs and cats. Here at Wooffurs we
+              also offer a social network for pet owners.
+            </span>
           </div>
         </div>
+        {/* <OutlinedCard /> */}
       </div>
-      <OutlinedCard />
     </div>
   );
 };
